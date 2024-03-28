9b7c21dad1db1290086324aec250dfbbecc166d3
Sahil Karn <skarn@emich.edu>
2024-03-28 01:26:46 +0000

    added pagination feature for this search query modified youtube.py to now fetch data from both page 1 and page 2 of youtube #4

--- youtube.py
+++ youtube.py
@@ -9,7 +9,7 @@ DEVELOPER_KEY = config.YOUTUBE_API_KEY
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-def youtube_search(query_term, max_results):
+def youtube_search(query_term, max_results, page_token=None):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
     # Call the search.list method to retrieve videos matching the specified query term.
@@ -17,21 +17,35 @@ def youtube_search(query_term, max_results):
         q=query_term,
         part="snippet",
         type="video",
-        maxResults=max_results
+        maxResults=max_results,
+        pageToken=page_token  # Include page token for pagination
     ).execute()
 
     # Extract the videos from the search response
     videos = search_response.get("items", [])
+    next_page_token = search_response.get("nextPageToken")
 
-    return videos
+    return videos, next_page_token
 
 if __name__ == "__main__":
     # Prompt the user for the search query and max results
     query_term = input("Enter the search query: ")
     max_results = int(input("Enter the maximum number of results: "))
 
-    # Perform the YouTube search
-    video_list = youtube_search(query_term, max_results)
+    # Perform the YouTube search for the first page
+    first_page_videos, next_page_token = youtube_search(query_term, max_results)
 
-    # Print the list of videos
-    print(video_list)
+    # Print the list of videos for the first page
+    print("First Page Results:")
+    for video in first_page_videos:
+        print(video)
+
+    # If there's a next page token, fetch the second page results
+    if next_page_token:
+        # Perform the YouTube search for the second page
+        second_page_videos, _ = youtube_search(query_term, max_results, next_page_token)
+
+        # Print the list of videos for the second page
+        print("\nSecond Page Results:")
+        for video in second_page_videos:
+            print(video)
